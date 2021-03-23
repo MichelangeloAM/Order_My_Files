74ce080b1e1ef1f8b98cf7a3e4b95f0da5c3d6ea
Michelangelo <michelangeloam98@gmail.com>
2021-03-23 16:06:40 +0100

    - Added buttons for selection of ordering type; - When one clicks a button, the function assigned to it will be called and then the program will be terminated.

--- Order_My_Files.py
+++ Order_My_Files.py
@@ -5,6 +5,8 @@ from os import rename
 from tkinter import simpledialog
 import getpass
 
+root = tkinter.Tk()
+
 # File types list
 ftypes = [
     ('Python code files', '*.py'),
@@ -17,9 +19,8 @@ ftypes = [
     ('All files', '*'),
 ]
 
+
 # The function renames and relocates selected files depending on the sorting selected
-
-
 def creation():
     lst.sort(key=os.path.getctime)
     num = 1
@@ -47,17 +48,43 @@ def lastaccessed():
         num += 1
 
 
+def hide(root):
+    root.iconify()
 
-root = tkinter.Tk()
+
+def show(root):
+    root.update()
+    root.iconify()
+
+
+def close():
+    root.destroy()
+
+
+def buttons():
+    root.geometry('400x400')
+    root.title('Select Ordering Mode')
+
+
+    btncreation = tkinter.Button(root, text='Order by Creation',
+                                 command=lambda: [creation(), close()])
+    btncreation.pack(side='top')
+    btncreation.place(relx=0.5, rely=0.0, anchor='n')
+
+    btnmodified = tkinter.Button(root, text='Order by Last Modified',
+                                 command=lambda: [lastmodified(), close()])
+    btnmodified.pack(side='top')
+    btnmodified.place(relx=0.5, rely=0.5, anchor='center')
+
+    btnaccessed = tkinter.Button(root, text='Order by Last Accessed',
+                                 command=lambda: [lastaccessed(), close()])
+    btnaccessed.pack(side='top')
+    btnaccessed.place(relx=0.5, rely=1.0, anchor='s')
+
+hide(root)
 
 # Window to select files to order
-filez = tkinter.filedialog.askopenfilenames(parent=root,
-                                            title='Select files to rename (must be all of the same type!)')
-
-'''
-for line in filez:
-    time.ctime(os.path.getctime(line))
-'''
+filez = tkinter.filedialog.askopenfilenames(title='Select files to rename (must be all of the same type!)')
 
 # List populated with file names
 lst = list(filez)
@@ -65,14 +92,7 @@ lst = list(filez)
 # Saves the extension of the selected files
 suffix = pathlib.Path(lst[0]).suffix
 
-
 # Window to get user input on file naming
-root = tkinter.Tk()
-root.geometry("400x240")
-
-ROOT = tkinter.Tk()
-
-ROOT.withdraw()
 
 # Asks user for file names, while loop won't break if no name is given
 USER_INP = simpledialog.askstring(title="File Names",
@@ -102,16 +122,8 @@ newpath = r'/Users/' + username + '/Desktop/' + foldername + '/'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
-user_input3 = simpledialog.askstring(title="Mode",
-                                     prompt="Write: c to order by creation, m by last modified, a by last accessed")
+show(root)
+buttons()
 
-while user_input3 != 'c' and user_input3 != 'm' and user_input3 != 'a':
-    user_input3 = simpledialog.askstring(title="Mode",
-                                         prompt="Write: c to order by creation, m by last modified, a by last accessed")
 
-if user_input3 == 'c':
-    creation()
-elif user_input3 == 'm':
-    lastmodified()
-else:
-    lastaccessed()
+root.mainloop()
